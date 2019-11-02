1d5309533d240e3bfeaa2886428f09dfa8912568
Bartek Gładecki <bgladecki@gmail.com>
2019-11-02 19:08:37 +0100

    validations for user locales inclusion in Gettext.known_locales

--- lib/pjeski/users/user.ex
+++ lib/pjeski/users/user.ex
@@ -1,6 +1,7 @@
 defmodule Pjeski.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
+  import Ecto.Changeset
 
   schema "users" do
     pow_user_fields()
@@ -12,6 +13,13 @@ defmodule Pjeski.Users.User do
   def changeset(user_or_changeset, params) do
     user_or_changeset
     |> pow_changeset(params)
-    |> Ecto.Changeset.cast(params, [:locale])
+    |> cast(params, [:locale])
+    |> validate_inclusion(:locale, available_locales_atoms())
+  end
+
+  defp available_locales_atoms do
+    PjeskiWeb.Gettext
+    |> Gettext.known_locales()
+    |> Enum.map(&String.to_atom/1)
   end
 end
