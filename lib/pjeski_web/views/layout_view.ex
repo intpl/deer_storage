ae489799f981f0932273406a1cb8693cbf19a9e9
Bartek Gładecki <bgladecki@gmail.com>
2019-11-05 22:42:37 +0100

    LayoutView#build_title_for_user not to take empty displayed_name

--- lib/pjeski_web/views/layout_view.ex
+++ lib/pjeski_web/views/layout_view.ex
@@ -1,15 +1,11 @@
 defmodule PjeskiWeb.LayoutView do
   use PjeskiWeb, :view
 
-  def build_title_for_user(%{displayed_name: name}) when not is_nil(name) do
-    name <> " - " <> gettext("Clinic Management System")
-  end
-
   def build_title_for_user(_user) do
     gettext("Clinic Management System")
   end
 
-  def build_header_for_user(%{displayed_name: name}) when not is_nil(name) do
+  def build_header_for_user(%{displayed_name: name}) do
     name |> prepend_emoji
   end
 
