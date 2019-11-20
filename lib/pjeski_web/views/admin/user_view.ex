4d9c123d7ba625ff0dae81377a92a2b70d3a228f
Bartek Gładecki <bgladecki@gmail.com>
2019-11-20 10:59:35 +0100

    Minor design changes

--- lib/pjeski_web/views/admin/user_view.ex
+++ lib/pjeski_web/views/admin/user_view.ex
@@ -28,8 +28,6 @@ defmodule PjeskiWeb.Admin.UserView do
     link subscription.name, to: Routes.admin_subscription_path(conn, :show, subscription.id)
   end
 
-  def subscription_expires_datetime_for(_, %Pjeski.Users.User{subscription: nil}), do: gettext("empty")
-  def subscription_expires_datetime_for(conn, %Pjeski.Users.User{subscription: subscription}) do
-    link subscription.expires_on, to: Routes.admin_subscription_path(conn, :show, subscription.id)
-  end
+  def subscription_expires_datetime_for(%Pjeski.Users.User{subscription: nil}), do: gettext("empty")
+  def subscription_expires_datetime_for(%Pjeski.Users.User{subscription: subscription}), do: subscription.expires_on
 end
