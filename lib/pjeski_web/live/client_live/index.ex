cbf0295f04475960e42f517255a0cf0f2bd4628d
Bartek Gładecki <bgladecki@gmail.com>
2019-11-27 17:00:00 +0100

    translations and honor locale in ClientLive.Index

--- lib/pjeski_web/live/client_live/index.ex
+++ lib/pjeski_web/live/client_live/index.ex
@@ -7,16 +7,24 @@ defmodule PjeskiWeb.ClientLive.Index do
   def render(assigns), do: PjeskiWeb.ClientView.render("index.html", assigns)
 
   def mount(%{"query" => query, "pjeski_auth" => token}, socket) do
-    {:ok, clients} = search_clients(subscription_id_from_token(token), query)
+    user = user_from_live_session(token)
+    subscription_id = user.subscription_id
+
+    user.locale
+    |> Atom.to_string
+    |> Gettext.put_locale
+
+    {:ok, clients} = search_clients(subscription_id, query)
     {:ok, assign(socket, clients: clients, query: query, token: token)}
   end
 
   def handle_event("filter", %{"query" => query}, %{assigns: %{token: token}} = socket) when byte_size(query) <= 50 do
-    {:ok, clients} = search_clients(subscription_id_from_token(token), query)
+    user = user_from_live_session(token)
+    subscription_id = user.subscription_id
+
+    {:ok, clients} = search_clients(subscription_id, query)
     {:noreply, assign(socket, token: token, clients: clients)}
   end
-
-  defp subscription_id_from_token(token), do: user_from_live_session(token).subscription_id
 
   defp search_clients(nil, _), do: {:error, "invalid subscription id"} # this will probably never happen, but let's keep this edge case just in case
   defp search_clients(id, nil), do: {:ok, list_clients_for_subscription(id)}
