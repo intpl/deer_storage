98b5f151f948107416cd9d3a7b3cc9e6c51eaa94
Bartek Gładecki <bgladecki@gmail.com>
2019-12-03 16:15:13 +0100

    display currently displayed client differently

--- lib/pjeski_web/views/client_view.ex
+++ lib/pjeski_web/views/client_view.ex
@@ -1,3 +1,7 @@
 defmodule PjeskiWeb.ClientView do
   use PjeskiWeb, :view
+
+  def classes_for_client_box(_, %{id: client_id}, %{id: client_id}), do: "has-background-link has-text-white"
+  def classes_for_client_box(current_user_id, %{user_id: current_user_id}, _), do: "has-background-white has-text-link is-clickable"
+  def classes_for_client_box(_, _, _), do: "has-background-light has-text-link is-clickable"
 end
