b9885b33d66e44ee5560483789d5745343cc9b62
Bartek Gładecki <bgladecki@gmail.com>
2020-03-15 06:47:42 +0100

    clean up migrations

--- priv/repo/migrations/20191102143340_alter_users_with_locale.exs
+++ priv/repo/migrations/20191102143340_alter_users_with_locale.exs
@@ -4,7 +4,6 @@ defmodule Pjeski.Repo.Migrations.AlterUsersWithLocales do
   def change do
     alter table(:users) do
       add :locale, :string, default: "en", null: false
-      add :expiry_date, :naive_datetime
     end
   end
 end
--- priv/repo/migrations/20191109093408_remove_expiry_date_from_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule Pjeski.Repo.Migrations.RemoveExpiryDateFromUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      remove :expiry_date
-    end
-  end
-end
