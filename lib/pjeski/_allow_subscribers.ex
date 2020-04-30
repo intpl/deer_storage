eca9b10773c796520c38912cf4a2d4c95b785aec
Bartek Gładecki <bgladecki@gmail.com>
2020-04-30 20:43:30 +0200

    use bang fnc for broadcasting events

--- lib/pjeski/_allow_subscribers.ex
+++ lib/pjeski/_allow_subscribers.ex
@@ -4,7 +4,7 @@ defmodule Pjeski.AllowSubscribers do
       def subscribe, do: :ok = Phoenix.PubSub.subscribe(Pjeski.PubSub, unquote(inspect(module)))
 
       def notify_subscribers({:ok, result}, event) do
-        Phoenix.PubSub.broadcast(Pjeski.PubSub, unquote(inspect(module)), {unquote(module), event, result})
+        Phoenix.PubSub.broadcast!(Pjeski.PubSub, unquote(inspect(module)), {unquote(module), event, result})
 
         {:ok, result}
       end
