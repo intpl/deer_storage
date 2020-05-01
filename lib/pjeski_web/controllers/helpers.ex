932e51d2c8b9669289c92344964fac23f8e75107
Bartek Gładecki <bgladecki@gmail.com>
2020-05-01 13:50:37 +0200

    admin dashboard after login in

--- lib/pjeski_web/controllers/helpers.ex
+++ lib/pjeski_web/controllers/helpers.ex
@@ -2,8 +2,7 @@ defmodule PjeskiWeb.Controllers.Helpers do
   alias PjeskiWeb.Router.Helpers, as: Routes
   alias Pjeski.Users.User
 
-  def dashboard_path_for(%User{subscription_id: nil, role: "admin"}), do: dashboard_path_for("admin")
-  def dashboard_path_for(%User{subscription_id: _, role: "admin"}), do: dashboard_path_for("user")
+  def dashboard_path_for(%User{role: "admin"}), do: dashboard_path_for("admin")
   def dashboard_path_for(%User{role: role}), do: dashboard_path_for(role)
   def dashboard_path_for("user"), do: Routes.live_path(PjeskiWeb.Endpoint, PjeskiWeb.DashboardLive.Index)
   def dashboard_path_for("admin"), do: Routes.admin_live_path(PjeskiWeb.Endpoint, PjeskiWeb.Admin.DashboardLive.Index)
