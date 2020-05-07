4ce67af077c24a1737c15d537bee2e673fa774cf
Bartek Gładecki <bgladecki@gmail.com>
2020-05-07 13:01:25 +0200

    Display DeerTables in Navbar

--- lib/pjeski_web/live/subscription_navigation_live.ex
+++ lib/pjeski_web/live/subscription_navigation_live.ex
@@ -1,15 +1,17 @@
 defmodule PjeskiWeb.SubscriptionNavigationLive do
   use Phoenix.LiveView
 
-  def mount(:not_mounted_at_router, %{"subscription_id" => subscription_id}, socket) do
-    {:ok, assign(socket, subscription_id: subscription_id)}
+  def mount(:not_mounted_at_router, %{"subscription_id" => _subscription_id, "subscription_tables" => subscription_tables}, socket) do
+    {:ok, assign(socket, subscription_tables: subscription_tables)}
   end
 
   def render(assigns) do
     ~L"""
-      <a class="navbar-item">Fields</a>
-      <a class="navbar-item">For</a>
-      <a class="navbar-item">Subscription no <%= @subscription_id %></a>
+      <%= for %{name: table_name, id: table_id} <- @subscription_tables do %>
+        <a class="navbar-item" href="#<%= table_id %>">
+          <%= table_name %>
+        </a>
+      <% end %>
     """
   end
 end
