779c10e458cdbcd189ca218f1a4803b7e8b3e178
Bartek Gładecki <bgladecki@gmail.com>
2020-08-08 08:03:55 +0200

    dashboard / make table name clickable

--- lib/pjeski_web/live/deer_dashboard_live/deer_table_show_component.ex
+++ lib/pjeski_web/live/deer_dashboard_live/deer_table_show_component.ex
@@ -1,4 +1,5 @@
 defmodule PjeskiWeb.DeerDashboardLive.DeerTableShowComponent do
+  alias PjeskiWeb.Router.Helpers, as: Routes
   use Phoenix.LiveComponent
 
   import PjeskiWeb.Gettext
@@ -6,15 +7,21 @@ defmodule PjeskiWeb.DeerDashboardLive.DeerTableShowComponent do
   def render(%{table: %{id: table_id, name: table_name, deer_columns: deer_columns}, cached_count: cached_count} = assigns) do
     ~L"""
     <div>
-      <strong><%= table_name %></strong> (<%= cached_count %>)
-
-
       <%= if @editing_table_id == nil do %>
+        <strong>
+          <%= live_redirect "#{table_name}", to: Routes.live_path(@socket, PjeskiWeb.DeerRecordsLive.Index, table_id) %>
+        </strong>
+
+        (<%= cached_count %>)
+
         <%= if cached_count == 0 do %>
           <a phx-click="delete_table" phx-value-table_id="<%= table_id %>"><%= gettext("Delete") %></a>
         <% end %>
 
         <a phx-click="toggle_table_edit" phx-value-table_id="<%= table_id %>"><%= gettext("Edit") %></a>
+      <% else %>
+        <strong><%= table_name %></strong>
+        (<%= cached_count %>)
       <% end %>
 
       <br>
