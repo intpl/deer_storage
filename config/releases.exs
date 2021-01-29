92657a2d1d706d86bb69635a5c656d52edbf6bc6
Bartek Gładecki <bgladecki@gmail.com>
2021-01-29 14:42:14 +0100

    use fetch_env! instead of get_env in config/releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -6,22 +6,15 @@ import Config
 
 config :pjeski, Pjeski.Repo,
   # ssl: true,
-  username: System.get_env("PGUSER"),
-  password: System.get_env("PGPASSWORD"),
-  database: System.get_env("PGDATABASE"),
-  hostname: System.get_env("PGHOST"),
-  port: System.get_env("PGPORT") |> String.to_integer,
+  username: System.fetch_env!("PGUSER"),
+  password: System.fetch_env!("PGPASSWORD"),
+  database: System.fetch_env!("PGDATABASE"),
+  hostname: System.fetch_env!("PGHOST"),
+  port: System.fetch_env!("PGPORT") |> String.to_integer,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
 
 config :pjeski, PjeskiWeb.Endpoint,
   url: [host: {:system, "APP_HOST"}, port: 443, scheme: "https"],
   http: [port: 80],
   server: true,
-  secret_key_base: secret_key_base
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
