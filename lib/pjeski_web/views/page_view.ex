adb5f7fec69559c843e1cb495becaa83b3fafbeb
Bartek Gładecki <bgladecki@gmail.com>
2021-02-02 10:17:55 +0100

    gifs and fixes at landing page (Page#index)

--- lib/pjeski_web/views/page_view.ex
+++ lib/pjeski_web/views/page_view.ex
@@ -1,3 +1,11 @@
 defmodule PjeskiWeb.PageView do
   use PjeskiWeb, :view
+
+  def dashboard_link(%{assigns: %{current_user: %{role: "admin"}, current_subscription: nil}}) do
+    link gettext("Go back to Admin Panel..."), to: Routes.admin_live_path(PjeskiWeb.Endpoint, PjeskiWeb.Admin.DashboardLive.Index), class: "button is-link"
+  end
+
+  def dashboard_link(_conn) do
+    link gettext("Go back to your database..."), to: Routes.live_path(PjeskiWeb.Endpoint, PjeskiWeb.DeerDashboardLive.Index), class: "button is-link"
+  end
 end
