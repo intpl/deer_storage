41fd0dbdee5c3bb04de978c40fd7bfd8f8aebd01
Bartek Gładecki <bgladecki@gmail.com>
2021-02-22 18:24:41 +0100

    preview images inside subscription

--- lib/pjeski_web/live/deer_records_live/modal/preview_file_component.ex
+++ lib/pjeski_web/live/deer_records_live/modal/preview_file_component.ex
@@ -1,27 +1,18 @@
 defmodule PjeskiWeb.DeerRecordsLive.Modal.PreviewFileComponent do
+  alias PjeskiWeb.Router.Helpers, as: Routes
   use Phoenix.LiveComponent
   import PjeskiWeb.Gettext
 
-  def render(%{deer_file: %{original_filename: original_filename}} = assigns) do
+  def render(%{deer_file: %{id: file_id}, record_id: record_id} = assigns) do
     ~L"""
-      <div class="modal is-active" phx-drop-target="<%= assigns[:drop_target_ref] %>">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-          <header class="modal-card-head">
-            <p class="modal-card-title">
-              <%= original_filename %>
-            </p>
-            <a class="delete" aria-label="close" data-bulma-modal="close" href="#" phx-click="close_preview_modal"></a>
-          </header>
-
-          <section class="modal-card-body">
-
-          </section>
-
-          <footer class="modal-card-foot">
-            <a class="button" data-bulma-modal="close" href="#" phx-click="close_preview_modal"><%= gettext("Close") %></a>
-          </footer>
+      <div class="modal is-active">
+        <a class="modal-background" phx-click="close_preview_modal" style="cursor: default;"></a>
+        <div class="modal-content">
+          <p class="image">
+            <img src="<%= Routes.deer_files_path(@socket, :download_record, record_id, file_id) %>">
+          </p>
         </div>
+        <a class="modal-close is-large" data-bulma-modal="close" href="#" phx-click="close_preview_modal"><%= gettext("Close") %></a>
       </div>
     """
   end
