71bc6410494c36d427cd5b66d5cb1b3b9edfdc09
Bartek Gładecki <bgladecki@gmail.com>
2021-02-24 16:44:52 +0100

    additional fixes for preview file component

--- lib/pjeski_web/live/deer_records_live/modal/preview_file_component.ex
+++ lib/pjeski_web/live/deer_records_live/modal/preview_file_component.ex
@@ -8,24 +8,24 @@ defmodule PjeskiWeb.DeerRecordsLive.Modal.PreviewFileComponent do
       <div class="modal is-active" phx-window-keydown="key" phx-target="<%= @myself %>" phx-hook="hookPreviewGestures">
         <a class="modal-background" phx-click="close_preview_modal" phx-target="<%= @myself %>" style="cursor: default;"></a>
 
-        <div class="buttons has-addons is-centered is-hidden-mobile">
-          <a class="button is-dark" phx-click="previous" phx-target="<%= @myself %>" href="#">
-            <%= gettext("Previous") %>
+        <div class="buttons has-addons is-centered is-hidden-mobile mb-0" title="<%= gettext("You can use keyboard arrows instead of clicking these buttons") %>">
+          <a class="button is-small is-dark mb-0" phx-click="previous" phx-target="<%= @myself %>" href="#">
+            <%= pgettext("image", "Previous") %>
           </a>
-          <a class="button is-dark" phx-click="next" phx-target="<%= @myself %>" href="#">
-            <%= gettext("Next") %>
+          <a class="button is-small is-dark mb-0" phx-click="next" phx-target="<%= @myself %>" href="#">
+            <%= pgettext("image", "Next") %>
           </a>
         </div>
-        <div class="has-text-white modal-content has-text-centered" id="divLoading">
+        <div class="has-text-white modal-content has-text-centered hide-overflow" id="divLoading">
           <br> <%= gettext("Loading") %>...
         </div>
 
         <p class="image">
-          <img id="imagePreview" src="<%= @image_url %>" class="image-inside-modal-overwrite">
+          <img id="imagePreview" src="<%= @image_url %>" class="image-inside-modal-overwrite" title="<%= @original_filename %>">
         </p>
 
-        <div class="modal-content has-text-centered">
-          <span class="has-text-white"><%= maybe_shrink_filename(@original_filename, 30) %></span>
+        <div class="modal-content has-text-centered hide-overflow">
+          <span class="has-text-white" title="<%= @original_filename %>"><%= maybe_shrink_filename(@original_filename, 30) %></span>
         </div>
         <a class="modal-close is-large" data-bulma-modal="close" href="#" phx-target="<%= @myself %>" phx-click="close_preview_modal"><%= gettext("Close") %></a>
       </div>
