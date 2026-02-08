fa82f6e021acb341c7f4bebaecd40f741806aadb
Bartek <bgladecki@gmail.com>
2026-02-08 15:22:16 +0700

    proudly update Elixir version yay

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule DeerStorage.MixProject do
   def project do
     [
       app: :deer_storage,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.1.1",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
